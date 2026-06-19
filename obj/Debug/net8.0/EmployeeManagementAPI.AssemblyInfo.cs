99c802ca5d37dbfa798335d9d513efa1e501a68a
Falguni4-deep <falgunichhatriya@gmail.com>
2026-06-19 22:25:31 +0530

    Support Render environment variables

--- obj/Debug/net8.0/EmployeeManagementAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/EmployeeManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5247df0020c8d4d4b1c96d2bfbfc4edc37cbc61c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a591d6a11d827d48afd4f6aeb7e87d4fe23568a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
